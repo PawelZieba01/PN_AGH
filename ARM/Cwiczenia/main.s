970bddf44da86cacc357128df7971ac9cf8df190
PawelZieba01 <zpawel.zieba@gmail.com>
2022-11-19 22:21:11 +0100

    Add ARM cw4

--- ARM/Cwiczenia/main.s
+++ ARM/Cwiczenia/main.s
@@ -7,15 +7,26 @@ __use_two_region_memory
 		EXPORT			__main
 		EXPORT			__use_two_region_memory
 		
+		
+		
+		
+		;---------------- initialise pins (4-7 in port 0) as output -----------------
+		LDR 	R5, =IODIR0			;load R5 with IODIR0 address
+		LDR 	R6, =2_11110000		;load R6 with pins directions	(binary)
+		STR 	R6, [R5]			;store R6 in addr OF R5 		(set outputs in PORT0)
+		;----------------------------------------------------------------------------
+		
+		
+		
 main_loop ;--------------------------------------------------------------------------
 		
-		LDR R0, =10				;load R0 with 10 				(miliseconds to wait)
-		BL delay_in_ms			;call delay_in_ms subroutine 	(wait)
+		LDR 	R0, =10				;load R0 with 10 				(miliseconds to wait)
+		BL 		delay_in_ms			;call delay_in_ms subroutine 	(wait)
 		NOP
 		NOP
 		NOP
 		NOP
-		b				main_loop
+		b 		main_loop
 		;----------------------------------------------------------------------------
 
 		
@@ -28,13 +39,13 @@ main_loop ;---------------------------------------------------------------------
 ;internal:		R1
 ;********************************
 delay_in_ms			
-		PUSH {R1,LR}
-		LDR R1, =15000					;load R1 with 14999 (number of loop repetition)
-		MUL R1, R0, R1					;multiplication R1 = R0xR1
+		PUSH 	{R1,LR}
+		LDR 	R1, =15000					;load R1 with 14999 (number of loop repetition)
+		MUL 	R1, R0, R1					;multiplication R1 = R0xR1
 loop1ms
-		SUBS R1, R1, #1 				;decrement R1
-		BNE loop1ms						;repeat loop1ms if R1 != 0
-		POP {R1, PC}
+		SUBS 	R1, R1, #1 					;decrement R1
+		BNE 	loop1ms						;repeat loop1ms if R1 != 0
+		POP		{R1, PC}
 ;******** END SUBROUTINE ********
 
 		END
