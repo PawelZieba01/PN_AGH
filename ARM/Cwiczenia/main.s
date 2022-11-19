78d399f8a981db5aebf025ad6ba3f1dacf73d0dd
PawelZieba01 <zpawel.zieba@gmail.com>
2022-11-19 21:21:43 +0100

    Add ARM cw3

--- ARM/Cwiczenia/main.s
+++ ARM/Cwiczenia/main.s
@@ -8,19 +8,34 @@ __use_two_region_memory
 		EXPORT			__use_two_region_memory
 		
 main_loop ;--------------------------------------------------------------------------
-
-
-		LDR R0, =50						;load R0 with number of miliseconds
-		LDR R1, =15000					;load R1 with 15000 (number of loop repetition)
-		MUL R1, R0,R1					;multiplication R1 = R0xR1
-loop1ms
-		SUBS R1, R1, #1 				;decrement R1
-		BNE loop1ms						;repeat loop1ms if R1 != 0
 		
-		
+		LDR R0, =10				;load R0 with 10 				(miliseconds to wait)
+		BL delay_in_ms			;call delay_in_ms subroutine 	(wait)
+		NOP
+		NOP
+		NOP
 		NOP
 		b				main_loop
 		;----------------------------------------------------------------------------
+
+		
+		
+		
+		
+;*** delay_in_ms [Subroutine] ***
+;in: 			R0 - number of miliseconds to wait
+;out 			-
+;internal:		R1
+;********************************
+delay_in_ms			
+		PUSH {R1,LR}
+		LDR R1, =15000					;load R1 with 14999 (number of loop repetition)
+		MUL R1, R0, R1					;multiplication R1 = R0xR1
+loop1ms
+		SUBS R1, R1, #1 				;decrement R1
+		BNE loop1ms						;repeat loop1ms if R1 != 0
+		POP {R1, PC}
+;******** END SUBROUTINE ********
 
 		END
 		
